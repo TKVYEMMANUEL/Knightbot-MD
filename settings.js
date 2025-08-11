80aa3e8d3ddd55d3b9a3548c795255a8eeef82bd
TKVYEMMANUEL <olusojiemmanuel58@gmail.com>
2025-08-12 00:24:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // king reon 
-  ownerNumber: '919876543210', //2347037618149
+  botOwner: 'king reon', // king reon 
+  ownerNumber: '2347037618149', //2347037618149
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
 };
